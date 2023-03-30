bdbe147304513ed6dbd22bfa1dc7acf0522a7d83
Elisa3331 <128787837+Elisa3331@users.noreply.github.com>
2023-03-30 13:26:40 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,12 +5,11 @@ int main(){
 float a=0;
 float b=0;
 float c=0;
-
 cin >> a >> b >> c;
 
-cout << (a*b)/2<<endl;
-cout << (a*a) <<endl;
-cout << (a*b) <<endl;
-cout << (((a+b)*c)/2) <<enld;
+cout<< (a*b)/2 <<endl;
+cout<< (a*a) <<endl;
+cout<< (a*b) <<endl;
+cout<< (((a+b)*c)/2) <<endl;
 
 }
